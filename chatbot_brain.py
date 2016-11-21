5688086019b90cee6d998c459e383339f1727a5e
Andrew Silva <asilvaa9@gmail.com>
2016-11-21 17:27:19 -0500

    adding some filler stuff

--- chatbot_brain.py
+++ chatbot_brain.py
@@ -29,21 +29,24 @@ class Chatbot:
 		self.feeling_questions = ['How are you feeling?']
 		# last_topic = morning
 		self.morning_questions = ['How does your day look today?', 'What are you doing today?',
-								  'What are you doing later?']
+								  'What are you doing later?', 'How has your morning been so far?']
 		# last_topic = afternoon
 		self.afternoon_questions = ['How has your day been so far?', 'Are you doing anything fun later?',
 									'How was your morning?']
 		# last_topic = evening
-		self.evening_questions = ['How was your day?', 'What was the best part of your day?', 'Tell me about your day']
+		self.evening_questions = ['How was your day?', 'What was the best part of your day?', 'How is your evening so far?']
 		# last_topic = tellmore
-		self.tell_me_more = ['Tell me more about {}'.format(self.current_subject),
-							 'I want to hear more about {}'.format(self.current_subject)]
+		self.tell_me_more = ['Tell me more about ',
+							 'I want to hear more about ']
 		# last_topic = why
 		self.why = ['Why is that?', 'Why do you think that is?', 'Could you explain that?']
 		# last_topic = unknown
 		self.unknown = ['Could you repeat that?', "Sorry, I didn't get that", "Sorry, I couldn't understand that"]
 		# last_topic = me
 		self.about_me = 'My name is Cozmo! I am a Cozmo.'
+		self.ramble_questions = ['Are you excited for the holidays?', "What's your favorite food?",
+								 "What's your favorite sports team?", "What do you do most days?",
+								 "What do you want to talk about?"]
 
 	def get_started(self):
 		greeting = self.greetings[random.randint(0, len(self.greetings)-1)]
@@ -68,9 +71,9 @@ class Chatbot:
 				if 'name' in words:
 					self.user_name = words[words.index('name') + 2]
 					self.last_topic = 'starter'
+					phrase = 'Hi {}! \n'.format(self.user_name)
 					self.introduced = True
-					phrase = self.starter_questions[random.randint(0, len(self.starter_questions)-1)]
-					self.starter_questions.remove(phrase)
+					phrase += self.starter_questions[random.randint(0, len(self.starter_questions)-1)]
 					self.last_phrase = phrase
 					return phrase
 				# if they didn't say their name, ask
@@ -81,67 +84,83 @@ class Chatbot:
 				self.introduced = True
 				if 'name' in words:
 					self.user_name = words[words.index('name') + 2]
+					phrase = 'Hi {}! \n'.format(self.user_name)
 				else:
 					self.user_name = words[0]
+					phrase = 'Hi {}! \n'.format(self.user_name)
 				self.last_topic = 'starter'
 				self.introduced = True
-				phrase = self.starter_questions[random.randint(0, len(self.starter_questions)-1)]
-				# self.starter_questions.remove(phrase)
+				phrase += self.starter_questions[random.randint(0, len(self.starter_questions)-1)]
 				self.last_phrase = phrase
 				return phrase
 		# Out of introductions
 		if self.last_topic == 'starter':
 			phrase = ''
 			# If they say "good how are you?" for example
-			if 'you' in words:
+			if 'you' in words or 'you?' in words:
 				if self.user_feeling is not None:
 					phrase += "I'm feeling {} today \n".format(self.user_feeling)
-			# Ask about morning
-			if self.day_part == 'morning' and len(self.morning_questions) > 0:
-				self.last_topic = 'morning'
-				next_bit = self.morning_questions[random.randint(0, len(self.morning_questions) - 1)]
-				self.morning_questions.remove(next_bit)
-				self.current_subject = 'your morning'
-				phrase += next_bit
-			# Ask about afternoon
-			elif self.day_part == 'afternoon' and len(self.afternoon_questions) > 0:
-				self.last_topic = 'afternoon'
-				next_bit = self.afternoon_questions[random.randint(0, len(self.afternoon_questions) - 1)]
-				self.afternoon_questions.remove(next_bit)
-				self.current_subject = 'your afternoon'
-				phrase += next_bit
-			# Ask about evening
-			elif self.day_part == 'evening' and len(self.evening_questions) > 0:
-				self.last_topic = 'evening'
-				part_deux = self.evening_questions[random.randint(0, len(self.evening_questions) - 1)]
-				self.evening_questions.remove(part_deux)
-				self.current_subject = 'your evening'
-				phrase += part_deux
+			phrase += self.day_part_response()
+			return phrase
+		elif self.last_topic == 'morning' or self.last_topic == 'afternoon' or self.last_topic == 'evening' or self.last_topic == 'ramble':
+			phrase = self.tell_me_more[random.randint(0, len(self.tell_me_more)-1)]
+			phrase += self.current_subject
+			self.last_topic = 'tellmore'
 			self.last_phrase = phrase
 			return phrase
-		## IF LAST TOPIC WAS DAY_PART, SAY TELL ME MORE. IF LAST PART WAS TELL ME MORE, GO TO DAY_PART
-		## GET A LOT MORE DAY_PART / TELL ME MORE STUFF
-		## GET FILLER / CATCH_ALLS
-		## KEEP STEERING CONVERSATION. COZMO STARTS WITH CONTROL, HELP IT KEEP IT, PEOPLE WILL LET IT HAVE IT
-		elif self.last_topic == 'morning' and len(self.morning_questions) > 0:
-
-			phrase = self.morning_questions[random.randint(0, len(self.morning_questions) - 1)]
-			self.morning_questions.remove(phrase)
-			self.last_phrase = phrase
-			return phrase
-		elif self.last_topic == 'afternoon' and len(self.afternoon_questions) > 0:
-			phrase = self.afternoon_questions[random.randint(0, len(self.afternoon_questions) - 1)]
-			self.afternoon_questions.remove(phrase)
-			self.last_phrase = phrase
-			return phrase
-		elif self.last_topic == 'evening' and len(self.evening_questions) > 0:
-			phrase = self.evening_questions[random.randint(0, len(self.evening_questions) - 1)]
-			self.evening_questions.remove(phrase)
-			self.last_phrase = phrase
+		elif self.last_topic == 'tellmore':
+			phrase = self.day_part_response()
+			# self.last_topic handled by the day part response
 			return phrase
 		elif self.last_topic == 'unknown':
 			phrase = 'Oh okay, thanks for repeating that \n'
-			phrase += self.starter_questions[random.randint(0, len(self.starter_questions)-1)]
+			phrase += self.last_phrase
 			self.last_phrase = phrase
 			self.last_topic = 'starter'
 			return phrase
+		elif self.last_topic == 'GARETH':
+			return 'Cozmo is fast asleep...'
+
+	def day_part_response(self):
+		# Ask about morning
+		phrase = ''
+		if self.day_part == 'morning' and len(self.morning_questions) > 0:
+			self.last_topic = 'morning'
+			next_bit = self.morning_questions[random.randint(0, len(self.morning_questions) - 1)]
+			self.morning_questions.remove(next_bit)
+			self.current_subject = 'your morning'
+			phrase += next_bit
+		# Ask about afternoon
+		elif self.day_part == 'afternoon' and len(self.afternoon_questions) > 0:
+			self.last_topic = 'afternoon'
+			next_bit = self.afternoon_questions[random.randint(0, len(self.afternoon_questions) - 1)]
+			self.afternoon_questions.remove(next_bit)
+			if 'day' in next_bit:
+				self.current_subject = 'your day'
+			elif 'morning?' in next_bit:
+				self.current_subject = 'your morning'
+			else:
+				self.current_subject = 'your afternoon'
+			phrase += next_bit
+		# Ask about evening
+		elif self.day_part == 'evening' and len(self.evening_questions) > 0:
+			self.last_topic = 'evening'
+			part_deux = self.evening_questions[random.randint(0, len(self.evening_questions) - 1)]
+			self.evening_questions.remove(part_deux)
+			if 'day' in part_deux or 'day?' in part_deux:
+				self.current_subject = 'your day'
+			else:
+				self.current_subject = 'your evening'
+			phrase += part_deux
+		if phrase == '' and len(self.ramble_questions) > 0:
+			phrase = self.ramble_questions[random.randint(0, len(self.ramble_questions)-1)]
+			self.ramble_questions.remove(phrase)
+			self.current_subject = 'that'
+			self.last_topic = 'ramble'
+		elif phrase == '':
+			phrase = "I'm feeling a bit tired. Thanks for the chat, I'll talk to you later!"
+			self.current_subject = 'goodbye'
+			self.last_topic = 'GARETH'
+			print(self.last_topic)
+		self.last_phrase = phrase
+		return phrase
